0ed8925abb453885a3bdd6518439a0f4bad7a1c4
Aline Rosa <alinerosa_s@hotmail.com>
2026-04-21 22:13:37 -0300

    [14]: Criando entidades

---
+++ FitProgress/FitProgress.Domain/Entities/PhysicalRecord.cs
@@ -0,0 +1,15 @@
+﻿namespace FitProgress.Domain.Entities
+{
+    public class PhysicalRecord
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime RecordDate { get; set; }
+        public decimal Weight { get; set; }
+        public string? Notes { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        public User User { get; set; } = null!;
+    }
+}
