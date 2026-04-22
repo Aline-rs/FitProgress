b6f6bbc9df764a1298601d88845fe7f7f4fca86a
Cleyton Lima <cleytonlima1516@gmail.com>
2026-04-22 20:51:04 -0300

    [17]: Criação do DTO UserDTO

---
+++ FitProgress/FitProgress.Api/DTO/UserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace FitProgress.Api.DTO
+{
+    public class UserDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
