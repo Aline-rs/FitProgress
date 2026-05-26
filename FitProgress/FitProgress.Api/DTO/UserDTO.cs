d9e5dd445c702ceda8f89d0fecef5b3a7b8d489f
Cleyton Lima <cleytonlima1516@gmail.com>
2026-05-26 20:17:24 -0300

    [17]: Criando UserDTO

--- FitProgress/FitProgress.Api/DTO/UserDTO.cs
+++ FitProgress/FitProgress.Api/DTO/UserDTO.cs
@@ -1,9 +1,16 @@
-﻿namespace FitProgress.Api.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitProgress.Api.DTO
 {
     public class UserDTO
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [MinLength(6)]
         public string Password { get; set; }
     }
 }
